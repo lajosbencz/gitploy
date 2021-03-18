76dd70548827d29f4357c3a9b5c2c350c71a4147
lazos <lazos@lazos.me>
2021-03-18 14:02:31 +0100

    remote other routes

--- main.go
+++ main.go
@@ -7,24 +7,6 @@ import (
 	"os"
 )
 
-func handleInit(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("InitHandler: " + r.RequestURI)
-	query := r.URL.Query()
-	remote := query.Get("remote")
-	local := query.Get("local")
-	err := os.RemoveAll(local)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	err = gitClone(remote, local)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	w.Write([]byte(fmt.Sprintf("remote:\t%s\nlocal:\t%s", remote, local)))
-}
-
 func main() {
 	log.Println(os.Args)
 	configFile := "config.yaml"
@@ -47,21 +29,6 @@ func main() {
 		}
 		hookHandler.handleHook(w, r)
 	})
-	http.HandleFunc("/init", handleInit)
 	fmt.Println("listening on " + config.Listen)
 	http.ListenAndServe(config.Listen, nil)
 }
-
-// func longRunningTask(sleep time.Duration) <-chan int32 {
-// 	r := make(chan int32)
-
-// 	go func() {
-// 		defer close(r)
-
-// 		// Simulate a workload.
-// 		time.Sleep(sleep)
-// 		r <- rand.Int31n(100)
-// 	}()
-
-// 	return r
-// }
